3422bac182d6d579429921334c942fced9292cc9
Reenadivya <reenad286@gmail.com>
2022-09-27 12:36:00 +0800

    fixed autoplay issue in viewers comp

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -6,45 +6,50 @@ const Viewers = (props) => {
       <Wrap>
         <img src="/images/viewers-disney.png" alt=""></img>
         <video
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
           src="videos/1564674844-disney.mp4"
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop
         ></video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt=""></img>
         <video
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop
           src="/videos/1564676714-pixar.mp4"
         ></video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt=""></img>
         <video
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop
           src="/videos/1564676115-marvel.mp4"
         ></video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt=""></img>
         <video
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop
           src="/videos/1608229455-star-wars.mp4"
         ></video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt=""></img>
         <video
-          autoPlay={true}
-          loop={true}
-          playsInline={true}
+          muted
+          autoPlay={"autoplay"}
+          preLoad="auto"
+          loop
           src="/videos/1564676296-national-geographic.mp4"
         ></video>
       </Wrap>
@@ -95,7 +100,7 @@ const Wrap = styled.div`
     position: absolute;
     top: 0px;
     left: 0px;
-    opacity: 0;
+    opacity: 1;
     z-index: 0;
   }
 
